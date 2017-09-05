6d670172edb1e435c0eba10d6c5d21222d9d4b9f
Velislav Velkov <velislav.velkov@egt-bg.com>
2017-09-05 10:15:37 +0300

    [Source] add data folder.

--- ImagesWidget.cpp
+++ ImagesWidget.cpp
@@ -8,15 +8,15 @@ ImagesWidget::ImagesWidget ( int maxGuesses, Wt::WContainerWidget *parent ) : Wt
 {
     for ( int i = 0; i <= maxGuesses; ++i )
     {
-        std::string fname = "hangman";
-        fname += boost::lexical_cast<std::string> ( i ) + ".png";
+        std::string fname = "data/hangman";
+        fname += boost::lexical_cast<std::string> ( i ) + ".jpg";
         Wt::WImage *theImage = new Wt::WImage ( fname, this );
         images_.push_back ( theImage );
 
         theImage->hide();
     }
 
-    Wt::WImage *hurray = new Wt::WImage ( "hangmanhurray.jpg", this );
+    Wt::WImage *hurray = new Wt::WImage ( "data/hangmanhurray.jpg", this );
     hurray->hide();
     images_.push_back ( hurray );
 
