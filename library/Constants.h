ee96588c935c4f07f2d24fc80e3945dc52dd4bf1
vivoood <vivoood@gmail.com>
2017-09-06 23:54:13 +0300

    [add constants]

---
+++ library/Constants.h
@@ -0,0 +1,5 @@
+#include <string>
+using namespace std;
+
+string strSiteName("Create real site name");
+string strSiteSlogan("use some slogan");
--- library/first_site/MainWidget.cpp
+++ library/first_site/MainWidget.cpp
@@ -1,13 +1,15 @@
 #include "MainWidget.h"
 
+#include "../Constants.h"
+
 #include <Wt/WTemplate>
 #include <Wt/WText>
 
 MainWidget::MainWidget ( Wt::WContainerWidget* perant ) : WContainerWidget ( perant )
 {
     Wt::WTemplate *templ = new Wt::WTemplate ( Wt::WString::tr ( "web-site-name-template" ) );
-    templ->bindWidget ( "web-site-name", new Wt::WText ( "Site's name" ) );
-    templ->bindWidget ( "web-site-slogan", new Wt::WText ( "Site's slogan" ) );
+    templ->bindWidget ( "web-site-name", new Wt::WText ( strSiteName ) );
+    templ->bindWidget ( "web-site-slogan", new Wt::WText ( strSiteSlogan ) );
     this->addWidget(templ);
 }
 
