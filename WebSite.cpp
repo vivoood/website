2f531f696dbe89b9d633b7f88dac126ed18892b1
vivoood <vivoood@gmail.com>
2017-09-06 14:39:16 +0300

    Use dl open.

--- WebSite.cpp
+++ WebSite.cpp
@@ -4,18 +4,54 @@
 
 #include <Wt/WApplication>
 
+#include <dlfcn.h>
 #include <string>
 
 #include "WebSite.h"
-#include "hangman/HangmanWidget.h"
+#include <hangman/HangmanWidget.h>
+#include <first_variant/MainWidget.h>
 
-WebSite::WebSite ( const Wt::WEnvironment& env ) : Wt::WApplication ( env )
+uint64_t WebSite::m_nInstanceCounter = 0;
+
+WebSite::WebSite ( const Wt::WEnvironment& env )
+    : Wt::WApplication ( env ),
+      m_pWebSite ( nullptr ),
+      m_handle ( nullptr ),
+      m_create ( nullptr ),
+      m_destroy ( nullptr )
+
 {
+    m_nInstanceCounter++;
+    m_nInstanceId = m_nInstanceCounter;
+    
     this->setTitle ( "Ivaylo Some Title" );
 
     this->messageResourceBundle().use ( this->appRoot() + "WebTemplates" );
     this->useStyleSheet ( "WebStyle.css" );
 
-    root()->addWidget ( new HangmanWidget ( std::string ( "Velko" ) ) );
+//     m_handle = dlopen ( "./libhangman.so", RTLD_LAZY );
+    m_handle = dlopen ( "./libfirst_variant.so", RTLD_LAZY );
+    if ( !m_handle )
+    {
+        std::cout << "OPEN: The error is " << dlerror() << std::endl;
+    }
+
+    m_create= ( create_t* ) dlsym ( m_handle,"create_object" );
+    m_destroy= ( destroy_t* ) dlsym ( m_handle,"destroy_object" );
+
+    if ( ( !m_create ) || ( !m_destroy ) )
+    {
+        std::cout << "USE: The error is " << dlerror() << std::endl;
+    }
+
+    m_pWebSite = m_create();
+    root()->addWidget ( m_pWebSite );
 }
+
+WebSite::~WebSite()
+{
+    m_destroy ( m_pWebSite );
+    dlclose ( m_handle );
+}
+
 // kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
--- WebSite.h
+++ WebSite.h
@@ -8,12 +8,24 @@
 namespace Wt
 {
 class WApplication;
+class WWidget;
 }
+
+typedef Wt::WWidget* create_t();
+typedef void destroy_t ( Wt::WWidget* );
 
 class WebSite : public Wt::WApplication
 {
 public:
     WebSite ( const Wt::WEnvironment& env );
+    virtual ~WebSite();
+private:
+    Wt::WWidget* m_pWebSite;
+    void* m_handle;
+    create_t* m_create;
+    destroy_t* m_destroy;
+    static uint64_t m_nInstanceCounter;
+    uint64_t m_nInstanceId;
 };
 
 #endif // WEBSITE_H
