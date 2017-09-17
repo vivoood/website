fc4b588a6f77f705581a6c5e3a5545d908a55f19
Home DELL <vivoood@gmail.com>
2017-09-18 00:10:07 +0300

    bind on clicked funktion.

--- library/primitives/WTabs.cpp
+++ library/primitives/WTabs.cpp
@@ -11,19 +11,23 @@ WTabs::WTabs ( Wt::WContainerWidget* parent ) : WContainerWidget ( parent )
 {
     Wt::WTemplate * templ = new Wt::WTemplate ( Wt::WString::tr ( "web-site-menu-template" ) );
 
+    Wt::WGroupBox *groupBox = new Wt::WGroupBox ( gConstants.vTabs.front().first );
+    groupBox->addWidget ( new Wt::WText ( "<p>Some contents.</p>" ) );
+    groupBox->addWidget ( new Wt::WText ( "<p>More contents.</p>" ) );
+    templ->bindWidget ( "groupbox", groupBox );
+
+    int margin = 2;
     for ( unsigned int i = 0; i < gConstants.vTabs.size(); i++ )
     {
         Wt::WPushButton * pBtn = new Wt::WPushButton ( gConstants.vTabs[i].first );
         pBtn->setStyleClass ( "web-site-menu-button-style" );
         templ->bindWidget ( gConstants.vTabs[i].second, pBtn );
-    }
 
-    Wt::WGroupBox *groupBox = new Wt::WGroupBox ( "A group box" );
-    groupBox->addWidget ( new Wt::WText ( "<p>Some contents.</p>" ) );
-    groupBox->addWidget ( new Wt::WText ( "<p>More contents.</p>" ) );
-    templ->bindWidget ( "groupbox", groupBox );
+        pBtn->clicked().connect ( std::bind ( [=]()
+        {
+            groupBox->setTitle ( pBtn->text() );
+        } ) );
+    }
 
     this->addWidget ( templ );
 }
-
-
