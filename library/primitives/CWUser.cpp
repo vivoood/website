969a35afa70f15e261dc400e335d4b77740ed410
Velislav Velkov <velislav.velkov@egt-bg.com>
2017-10-04 14:06:49 +0300

    CWUser added

---
+++ library/primitives/CWUser.cpp
@@ -0,0 +1,67 @@
+#include "CWUser.h"
+
+CWUser::CWUser ( std::string user, std::string sha, std::string mail, std::string coutry, std::string gender )
+{
+    _user = user;
+    _sha = sha;
+    _mail = mail;
+    _country = coutry;
+    _gender = gender;
+}
+
+void CWUser::save()
+{
+    std::ofstream outfile ( "users/" + _user );
+    outfile << *this;
+    outfile.close();
+}
+
+bool CWUser::load ( std::string filename )
+{
+    std::string dir ( "users/" );
+    dir.append ( filename );
+
+    if ( ! CWUser::CheckUserExist ( filename ) )
+        return false;
+
+    std::ifstream infile ( dir );
+    infile >> *this;
+    infile.close();
+
+    return true;
+}
+
+bool CWUser::CheckUserExist ( std::string filename )
+{
+    std::ifstream infile ( "users/" + filename );
+    bool bExist = infile.good();
+    infile.close();
+    return bExist;
+}
+
+std::ostream& operator<< ( std::ostream& os, const CWUser& dt )
+{
+    os << dt._user << "," << dt._sha << "," << dt._mail << "," << dt._country << "," << dt._gender;
+    return os;
+}
+
+std::istream& operator>> ( std::istream& is, CWUser& dt )
+{
+    std::string str;
+    is >> str;
+
+    std::istringstream ss ( str );
+    std::getline ( ss, dt._user, ',' );
+    std::getline ( ss, dt._sha, ',' );
+    std::getline ( ss, dt._mail, ',' );
+    std::getline ( ss, dt._country, ',' );
+    std::getline ( ss, dt._gender, ',' );
+
+    return is;
+}
+
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
+
+
+
+
---
+++ library/primitives/CWUser.h
@@ -0,0 +1,29 @@
+#ifndef CWUSER_H
+#define CWUSER_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+
+class CWUser
+{
+public:
+    CWUser() {}
+    CWUser ( std::string user, std::string sha, std::string mail, std::string coutry, std::string gender );
+    void save();
+    bool load ( std::string filename );
+    static bool CheckUserExist ( std::string filename );
+private:
+    std::string _user = "n/a";
+    std::string _sha = "n/a";
+    std::string _mail = "n/a";
+    std::string _country = "n/a";
+    std::string _gender = "n/a";
+    friend std::ostream& operator<< ( std::ostream& os, const CWUser& dt );
+    friend std::istream& operator>> ( std::istream& is, CWUser& dt );
+
+};
+
+#endif //CWUSER_H
+
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
